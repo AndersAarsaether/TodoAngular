3ff98d021841caa34705699dfc33dab536186c8f
584975 <584975@stud.hvl.no>
2022-10-03 10:13:15 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'TodoAngular';
+  todos: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    this.http.get('http://localhost:8080/todos').subscribe((todos: any) => {
+      this.todos = todos;
+    });
+  }
+
+  deleteTodo = (id: number) =>
+    this.http
+      .delete('http://localhost:8080/todos/' + id)
+      .subscribe((response: any) => console.log(response));
 }
